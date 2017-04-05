5c319df1672d7b1b3aa107b6504bf9c18a92447d
jbonaiuto <jbonaiuto@gmail.com>
2017-04-05 13:48:48 +0100

    refactoring

--- plot_surface_statistic_free_energy.m
+++ plot_surface_statistic_free_energy.m
@@ -11,108 +11,18 @@ for f = fieldnames(defaults)',
     end
 end
 
-out_dir=fullfile(params.plot_dir,statistic);
-mkdir(out_dir);
-
 methodnames={'EBB','IID','COH','MSP'}; %% just 1 method for now
 methods_to_plot=[1 4];
 Nmeth=length(methodnames);
 
-orig_white_mesh=fullfile(params.surf_dir,[subj_info.subj_id subj_info.birth_date '-synth'],'surf','white.hires.deformed.surf.gii');
-white_mesh=fullfile(params.surf_dir,[subj_info.subj_id subj_info.birth_date '-synth'],'surf','ds_white.hires.deformed.surf.gii');
+[pial_statistic,wm_statistic]=plot_surface_statistic(statistic, subj_info,...
+    'surf_dir', params.surf_dir, 'clip_vals', params.clip_vals, ...
+    'limits', params.limits, 'plot_dir', params.plot_dir);
 
-orig_pial_mesh=fullfile(params.surf_dir,[subj_info.subj_id subj_info.birth_date '-synth'],'surf','pial.hires.deformed.surf.gii');
-pial_mesh=fullfile(params.surf_dir,[subj_info.subj_id subj_info.birth_date '-synth'],'surf','ds_pial.hires.deformed.surf.gii');
-
-allmeshes=strvcat(white_mesh, pial_mesh);
-Nmesh=size(allmeshes,1);
-
-wm=gifti(white_mesh);
-pial=gifti(pial_mesh);
-wm_inflated=gifti(fullfile(params.surf_dir,[subj_info.subj_id subj_info.birth_date '-synth'],'surf','ds_white.hires.deformed_inflated.surf.gii'));
-pial_inflated=gifti(fullfile(params.surf_dir,[subj_info.subj_id subj_info.birth_date '-synth'],'surf','ds_pial.hires.deformed_inflated.surf.gii'));
-
-pial_white_map=map_pial_to_white(white_mesh, pial_mesh, ...
-        'mapType', 'link', 'origPial', orig_pial_mesh, ...
-        'origWhite', orig_white_mesh);
-    
-switch statistic
-    case 'thickness'
-        [pial_statistic, wm_statistic]=compute_thickness(pial_mesh, ...
-            white_mesh, orig_pial_mesh, orig_white_mesh);
-    case 'curvature'
-        pial_statistic=compute_curvature(pial_mesh, 'curvature_type', 'mean');
-        wm_statistic=compute_curvature(white_mesh, 'curvature_type', 'mean');
-    case 'depth'
-        [pial_statistic,HS]=compute_sulcal_depth(pial_mesh);
-        mapping=dsearchn(HS.vertices,wm.vertices);
-        wm_statistic=sqrt(sum((wm.vertices-HS.vertices(mapping,:)).^2,2));
-    case 'lead_field_norm'
-        D=spm_eeg_load('C:\Users\jbonai\Dropbox\meg\layer_sim\rgb_1_pial.mat');
-        pial_statistic=sqrt(sum(D.inv{1}.inverse.L.^2,1))';
-
-        D=spm_eeg_load('C:\Users\jbonai\Dropbox\meg\layer_sim\rgb_1_white.mat');
-        wm_statistic=sqrt(sum(D.inv{1}.inverse.L.^2,1))';
-end
-
-switch statistic
-    case 'thickness'
-        fig=figure();
-        bins=linspace(min(pial_statistic),max(pial_statistic),100);
-        [n,b] = histc(pial_statistic,bins);
-        bar(bins,n./sum(n)*100.0,'histc');
-        xlabel('Thickness');
-        ylabel('% Vertices');
-    otherwise
-        fig=figure();
-        plot(pial_statistic,wm_statistic(pial_white_map),'.');
-        hold on
-        min_val=min([pial_statistic(:) wm_statistic(:)]);
-        max_val=max([pial_statistic(:) wm_statistic(:)]);
-        plot([min_val max_val],[min_val max_val],'r-');
-        mean((pial_statistic-wm_statistic(pial_white_map))./wm_statistic(pial_white_map))
-        std((pial_statistic-wm_statistic(pial_white_map))./wm_statistic(pial_white_map))
-        xlabel(sprintf('Pial %s',statistic));
-        ylabel(sprintf('White matter %s',statistic));
-end
-%saveas(fig, fullfile(out_dir, sprintf('pial_white_%s.png', statistic)), 'png');
-%figure2eps(fig, fullfile(out_dir, sprintf('pial_white_%s.eps', statistic)), 10, '-opengl');
-        
-[ax, metric_data]=plot_surface_metric(pial, pial_statistic, 'clip_vals', params.clip_vals, 'limits', params.limits);
-set(ax,'CameraViewAngle',5.338);
-set(ax,'CameraTarget',[20.523 26.884 0.768]);
-set(ax,'CameraUpVector',[-0.052 0.926 0.375]);
-set(ax,'CameraPosition',[57.457 -626.649 1618.708]);
-fig=get(ax,'Parent');
-%saveas(fig, fullfile(out_dir, sprintf('pial_%s.png', statistic)), 'png');
-
-[ax, metric_data]=plot_surface_metric(pial_inflated, pial_statistic, 'clip_vals', params.clip_vals, 'limits', params.limits);
-set(ax,'CameraViewAngle',5.338);
-set(ax,'CameraTarget',[20.523 26.884 0.768]);
-set(ax,'CameraUpVector',[-0.052 0.926 0.375]);
-set(ax,'CameraPosition',[57.457 -626.649 1618.708]);
-fig=get(ax,'Parent');
-%saveas(fig, fullfile(out_dir, sprintf('pial_inflated_%s.png', statistic)), 'png');
-
-[ax, metric_data]=plot_surface_metric(wm, wm_statistic, 'clip_vals', params.clip_vals, 'limits', params.limits);
-set(ax,'CameraViewAngle',5.338);
-set(ax,'CameraTarget',[20.523 26.884 0.768]);
-set(ax,'CameraUpVector',[-0.052 0.926 0.375]);
-set(ax,'CameraPosition',[57.457 -626.649 1618.708]);
-fig=get(ax,'Parent');
-%saveas(fig, fullfile(out_dir, sprintf('wm_%s.png', statistic)), 'png');
-
-[ax, metric_data]=plot_surface_metric(wm_inflated, wm_statistic, 'clip_vals', params.clip_vals, 'limits', params.limits);
-set(ax,'CameraViewAngle',5.338);
-set(ax,'CameraTarget',[20.523 26.884 0.768]);
-set(ax,'CameraUpVector',[-0.052 0.926 0.375]);
-set(ax,'CameraPosition',[57.457 -626.649 1618.708]);
-fig=get(ax,'Parent');
-%saveas(fig, fullfile(out_dir, sprintf('wm_inflated_%s.png', statistic)), 'png');
-
-nverts=size(wm.vertices,1);
+nverts=size(pial_statistic,1);
 rng(0);
 simvertind=randperm(nverts); %% random list of vertex indices to simulate sources on
+Nmesh=2;
 
 for i=1:length(methods_to_plot),       
     methind=methods_to_plot(i);
@@ -122,10 +32,6 @@ for i=1:length(methods_to_plot),
     
     allTrueOtherF=zeros(Nmesh*params.nsims,1);
     for simmeshind=1:Nmesh,    
-        [path,file,ext]=fileparts(deblank(allmeshes(simmeshind,:)));
-        x=strsplit(file,'.');
-        y=strsplit(x{1},'_');
-        
         data_file=fullfile('D:\layer_sim\results\',subj_info.subj_id, num2str(session_num), sprintf('allcrossF_f%d_%d_SNR%d_dipolemoment%d.mat',freq(1),freq(2),params.snr,params.dipole_moment));
         load(data_file);
 
@@ -157,6 +63,6 @@ for i=1:length(methods_to_plot),
     ylabel('Free energy diff (correct-incorrect)');
     title(sprintf('Free energy, %s, Rho=%.5f, p=%.5f',methodnames{methind}, rho, pval));        
     
-    %saveas(fig, fullfile(out_dir, sprintf('%s_free_energy_%s_%d-%dHz_%ddb.png', statistic, method, freq(1), freq(2), params.snr)), 'png');
-    %figure2eps(fig, fullfile(out_dir, sprintf('%s_free_energy_%s_%d-%dHz_%ddb.eps', statistic, method, freq(1), freq(2), params.snr)), 10, '-opengl');
+    saveas(fig, fullfile(params.plot_dir, sprintf('%s_free_energy_%s_%d-%dHz_%ddb.png', statistic, method, freq(1), freq(2), params.snr)), 'png');
+    figure2eps(fig, fullfile(params.plot_dir, sprintf('%s_free_energy_%s_%d-%dHz_%ddb.eps', statistic, method, freq(1), freq(2), params.snr)), 10, '-opengl');
 end
